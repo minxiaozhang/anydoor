7eee104688e91c09c65ea5200e5c64ff7f1a741d
张敏 <zhangmin003>
2019-07-08 18:10:26 +0800

    优化代码添加文件夹连接，使用页面模板，handlebar

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const path=require('path');
 const  conf=require('./config/defaultConfig');
 const route = require('./helper/route');
 
+
 const  server=http.createServer((req,res)=>{
       const filePath=path.join(conf.root,req.url);
       route(req, res, filePath, filePath);
--- src/helper/route.js
+++ src/helper/route.js
@@ -2,6 +2,13 @@ const fs=require('fs')
 const promisify=require('util').promisify;
 const stat=promisify(fs.stat);
 const readdir=promisify(fs.readdir);
+const Handlebars=require('handlebars');
+const path=require('path');
+const  conf=require('../config/defaultConfig'); //使用require 的时候可以放心的时候相对路径
+//读取页面模板，只执行一次
+const tplPath=path.join(__dirname,'../template/dir.tpl')
+const  source=fs.readFileSync(tplPath);
+const template=Handlebars.compile(source.toString())
 
 module.exports = async function(req,res,filePath) {
   try {
@@ -13,8 +20,14 @@ module.exports = async function(req,res,filePath) {
     }else if(stats.isDirectory()){
       const files= await readdir(filePath);
       res.statusCode=200;
-      res.setHeader('Content-Type','text/plain;charset=utf-8');
-      res.end(files.join(','));
+      res.setHeader('Content-Type','text/html;charset=utf-8');
+      const dir=path.relative(conf.root,filePath);//一个路径相对于另一个路径的相对路径
+      const data={
+        title:path.basename(filePath),
+        dir:dir?`/${dir}`:'',
+        files,
+      }
+      res.end(template(data));
     }
   }catch (e) {
     console.error(e)
